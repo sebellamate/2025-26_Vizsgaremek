978a5a52a3a3659102ec25568270e330617ec0da
Adam5321 <95025468+Adam5321@users.noreply.github.com>
2025-11-07 09:06:47 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const app = express();
+
+const api = express();
+
+const cors = require("cors");
+
+const cookieParser = require("cookie-parser");
+
+app.use(cors(
+{
+    origin: [ "http://localhost:3000" ],
+    credentials: true,
+}));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+const userRoutes = require("./api/routes/userRoutes");
+
+const errorHandler = require("./api/middlewares/errorHandler");
+
+const authRoutes = require("./api/routes/authRoutes");
+
+const termekRoutes = require("./api/routes/termekRoutes")
+
+const kategoriaRoutes = require("./api/routes/kategoriaRoutes")
+const kosartetelekRoutes = require("./api/routes/kosartetelRoutes")
+
+app.use("/api", api);
+
+api.use("/users", userRoutes);
+
+api.use("/auth", authRoutes);
+
+api.use("/termekek", termekRoutes);
+
+api.use("/kategoriak", kategoriaRoutes);
+api.use("/kosartetelek", kosartetelekRoutes);
+
+api.use(errorHandler.notFound);
+
+app.use(errorHandler.showError);
+
+app.use(errorHandler.notFound);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,10 @@
+require("dotenv").config({ quiet: true });
+
+const app = require("./app");
+
+const PORT = process.env.PORT ?? 8000;
+
+app.listen(PORT, () => 
+{
+    console.log(`http://localhost:${PORT}`);
+});
